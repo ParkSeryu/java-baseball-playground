a10063b770579fe2ca97d974710ae92b6b6ff63f
ParkSeryu <pjh971212@gmail.com>
2024-07-22 14:13:13 +0900

    학습테스트 실습 완료

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,11 @@
 package study;
 
+import java.util.Arrays;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
@@ -10,4 +13,34 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    void replace2() {
+        String[] actual = "1,2".split(",");
+        assertThat(actual).contains("1", "2");
+    }
+
+
+    @Test
+    void replace3() {
+        String[] actual = "1".split(",");
+        assertThat(actual).containsExactly("1");
+    }
+
+    @Test
+    void replace4() {
+        String suit = "(1,2)";
+        String substring = suit.substring(1, suit.length() - 1);
+        assertThat(substring).isEqualTo("1,2");
+    }
+
+    @Test
+    void replace5() {
+        String suit = "abc";
+        assertThatThrownBy(() -> suit.charAt(suit.length() + 1))
+                .isInstanceOf(StringIndexOutOfBoundsException.class);
+    }
+
+
+
 }
